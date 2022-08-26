23b447ff0b98c5c1171df8e3515434e0f54d4c41
flgadams <flgadams@gmail.com>
2022-08-25 20:19:58 -0400

    Initial commit

---
+++ template-swift-no-sb/RootViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  template-swift-no-sb
+//
+//  Created by Glenn Adams on 7/30/22.
+// A Swift project template for UIKit apps, no storyboards
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemRed
+    }
+}
+
